d97a0b1862290097fe96dd444aeb9138890964db
danielbargao <daniel.bruno.duarte.bargao@gmail.com>
2020-05-31 17:11:12 +0100

    search bar only make the 1st line of the grid apear

--- app.js
+++ app.js
@@ -1,51 +1,79 @@
 const gridItems = [
-  
+
   {
+    id: 'covid',
     Issues: 'Covid-19',
     NumberOfAfectedPeople: '3 Milion',
     PriorityRank: 'High',
     BriefDescription: 'The virus that is making the hole world quarantine because there is no vaccine',
-    ViewDetails: `<a href="page2.html"><img src="images/view_details2-2.jpg.png" alt="view_details"
-    class="view_details"></a>`
   },
 
   {
+    id: 'shortage',
     Issues: 'Shortage of Goods',
     NumberOfAfectedPeople: '7.6 Bilion',
     PriorityRank: 'Medium',
     BriefDescription: 'People tend to buy things on impulse and now that has more impact than ever',
-    ViewDetails: `<a href="page2.html#Shortage-of-Goods-2"><img src="images/view_details2-2.jpg.png"
-    alt="view_details" class="view_details"></a>`
   },
 
   {
+    id: 'mental',
     Issues: 'Mental Health',
     NumberOfAfectedPeople: 'Not defined',
     PriorityRank: 'Low',
     BriefDescription: `Because we are quarantined and can't go out a lot of people are having mental health issues`,
-    ViewDetails: `<a href="page2.html#Mental-Health-2"><img src="images/view_details2-2.jpg.png"
-    alt="view_details" class="view_details"></a>`
   },
 
   {
+    id: 'pollution',
     Issues: 'Pollution',
     NumberOfAfectedPeople: '7.6 Bilion',
     PriorityRank: 'Low',
     BriefDescription: 'Pollution is a global issue that is really important in today society but is way less of an issue due to global quarantine',
-    ViewDetails: `<a href="page2.html#Pollution-2"><img src="images/view_details2-2.jpg.png"
-    alt="view_details" class="view_details"></a>`
   },
 
 ]
 
-const grid = document.querySelector("#flex-gird");
+const grid = document.querySelector("#flex-grid");
 
 for (let i = 0; i < gridItems.length; i++) {
-  grid.innerHTML += `<div id="item" class="item">
-     ${gridItems[i].Issues}
-     ${gridItems[i].NumberOfAfectedPeople}
-     ${gridItems[i].PriorityRank}
-     ${gridItems[i].BriefDescription}
-     ${gridItems[i].ViewDetails}
+    grid.innerHTML += `<div id="grid-items" class="grid-items">
+     <div class="grid-${gridItems[i].id}1" id="${gridItems[i].id}">${gridItems[i].Issues}</div>
+     <div>${gridItems[i].NumberOfAfectedPeople}</div>
+     <div>${gridItems[i].PriorityRank}</div>
+     <div>${gridItems[i].BriefDescription}</div>
+     <div><a href="page2.html#${gridItems[i].id}-2"><img src="images/view_details2-2.jpg.png"
+     alt="view_details" class="view_details"></div>
    </div>`;
 }
+
+function searchFilter() {
+  let input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+function showFuntion(){
+
+  let x = document.getElementById("grid-items");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+
+}
+
+console.log(grid)
